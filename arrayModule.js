247ff332187eecbf2645c591d2bfa9621aa4540e
AshtonPhillips2005 <ashtongamer11@gmail.com>
2024-10-19 00:42:43 -0400

    Add files via upload

---
+++ arrayModule.js
@@ -0,0 +1,11 @@
+
+export function findMax(arr) {
+    if (arr.length === 0) {
+      return null; 
+    }
+    return Math.max(...arr);
+  }
+  
+export function reverseArray(arr) {
+    return arr.slice().reverse(); 
+  }
